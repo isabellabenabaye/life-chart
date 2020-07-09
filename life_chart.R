83cf33d64dbd0c9fba17d3fe7416322c324a1a77
Isabella Benabaye <isabellabenabaye@gmail.com>
2020-07-09 13:29:04 -0700

    :tada: final chart + comments

--- life_chart.R
+++ life_chart.R
@@ -2,7 +2,10 @@ library(tidyverse)
 library(lubridate)
 library(waffle)
 library(hrbrthemes)
+library(extrafont)
+loadfonts(device = "win", quiet = TRUE)
 
+# Create the data-----
 life_data <-
   tibble(months = factor(rep(month.abb[1:12], 82), levels=month.abb[1:12])) %>%   ## make months
   slice(4:(n()-9)) %>%  ## remove months in 1996 before my birth month (April)
@@ -11,6 +14,7 @@ life_data <-
   ) %>%
   rowid_to_column("row_name")  ## add column for row number
 
+## add the "eras" to be colored in the waffle chart
 life_data <- life_data %>%
   mutate(era = fct_inorder(case_when(row_name < 161 ~ "Childhood - US",
                          row_name < 207 ~ "High School - PH",
@@ -19,11 +23,19 @@ life_data <- life_data %>%
                          row_name < ((year(Sys.Date()) - 1996)*12) + (month(Sys.Date()) - 3) ~ "Adulting - US", ## months into "Adulting - US" based on current month
                          TRUE ~ "Time left")))
 
-life_data %>%
-  count(era) %>%
+# Waffle chart-----
+life_waffle <- life_data %>%
+  count(era) %>% ## the count of each era is the number of months in that era
   ggplot(aes(fill = era, values = n)) +
-  geom_waffle(color = "white", n_rows = 12, flip = TRUE) +
-  scale_fill_manual(name = "", values = c("#EF476F","#FCA311","#FFD166","#0EAD69","#4ECDC4","#118AB2")) +
+  geom_waffle(color = "#F7F7F7", n_rows = 12, size = 1, flip = TRUE) + ## make each row a year/12 months
+  scale_fill_manual(name = "", values = c("#EF476F","#FCA311","#FFD166","#0EAD69","#4ECDC4","#118AB2")) +  ## assign colors to the eras
   coord_equal() +
-  theme_ipsum(base_family = "IBM Plex Sans", grid = "") +
+  theme_ipsum(grid = "") +
+  theme(legend.text = element_text(family = "Cooper Lt BT", size = 40),
+        plot.background = element_rect(fill = "#F7F7F7", color = "#F7F7F7")) +
   theme_enhance_waffle()
+
+life_waffle
+
+# Save the chart
+life_waffle + ggsave("life_waffle.png", device = "png", type = "cairo", width = 15, height = 25, dpi = 300)
