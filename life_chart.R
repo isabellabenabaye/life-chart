6827c023cbf59d5f3f2ab5b64319aa2c7703ca48
Isabella Benabaye <isabellabenabaye@gmail.com>
2020-07-09 14:26:50 -0700

    :zap: age limit 70 instead of 80

--- life_chart.R
+++ life_chart.R
@@ -7,21 +7,21 @@ loadfonts(device = "win", quiet = TRUE)
 
 # Create the data-----
 life_data <-
-  tibble(months = factor(rep(month.abb[1:12], 82), levels=month.abb[1:12])) %>%   ## make months
+  tibble(months = factor(rep(month.abb[1:12], 72), levels=month.abb[1:12])) %>%   ## make months
   slice(4:(n()-9)) %>%  ## remove months in 1996 before my birth month (April)
   tibble(
-    age = rep(0:80, each = 12) ## age range: 1-80
+    age = rep(0:70, each = 12) ## age range: 1-70
   ) %>%
   rowid_to_column("row_name")  ## add column for row number
 
 ## add the "eras" to be colored in the waffle chart
 life_data <- life_data %>%
   mutate(era = fct_inorder(case_when(row_name < 161 ~ "Childhood - US",
-                         row_name < 207 ~ "High School - PH",
-                         row_name < 255 ~ "College - PH",
-                         row_name < 285 ~ "Adulting - PH",
-                         row_name < ((year(Sys.Date()) - 1996)*12) + (month(Sys.Date()) - 3) ~ "Adulting - US", ## months into "Adulting - US" based on current month
-                         TRUE ~ "Time left")))
+                                     row_name < 207 ~ "High School - PH",
+                                     row_name < 255 ~ "College - PH",
+                                     row_name < 285 ~ "Adulting - PH",
+                                     row_name < ((year(Sys.Date()) - 1996)*12) + (month(Sys.Date()) - 3) ~ "Adulting - US", ## months into "Adulting - US" based on current month
+                                     TRUE ~ "Time left")))
 
 # Waffle chart-----
 life_in_months <- life_data %>%
